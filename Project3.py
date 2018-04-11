250b21a8d91d802abfe23427417351b75a79352f
pwkurth <38272066+pwkurth@users.noreply.github.com>
2018-04-10 23:51:49 -0500

    Add files via upload

---
+++ Project3.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+company_dict = {"0":"Company", "1":"Booth",
+                "2":"Full-Time", "3":"Full-Time Visa Sponsor",
+                "4":"Part-Time", "5":"Internship",
+                "6":"Freshman", "7":"Sophomore",
+                "8":"Junior", "9":"Senior",
+                "10":"Post-Bacs", "11":"MS",
+                "12":"PhD", "13":"Alumni",
+               }
+
